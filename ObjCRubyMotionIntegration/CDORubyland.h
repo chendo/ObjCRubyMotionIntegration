3cb18b425b0db107738586e8af398888af6d9fed
Jack Chen (chendo) <github@chen.do>
2013-05-19 13:24:59 +1000

    Add header for ruby class

---
+++ ObjCRubyMotionIntegration/CDORubyland.h
@@ -0,0 +1,17 @@
+//
+//  CDORubyland.h
+//  ObjCRubyMotionIntegration
+//
+//  Created by Jack Chen on 19/05/13.
+//  Copyright (c) 2013 Jack Chen. All rights reserved.
+//
+
+@interface CDORubyland : NSObject
+
+- (id)countdown;
+
+@end
+
+@implementation CDORubyland
+
+@end
